caa18f3a54d291b6061dee6850bef2710a3f98bc
Patil, Hanamant <hanamant5496@gmail.com>
2025-07-11 16:00:40 +0530

    ADDED VEHICLE MANAGEMENET MODULE

---
+++ Assignment7/Controllers/VehicleEntryExitManagementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class VehicleEntryExitManagementController : Controller
+    {
+        public IActionResult VehicleEntryExitManagement()
+        {
+            return View();
+        }
+    }
+}
