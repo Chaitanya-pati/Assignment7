632e1b3a2434ae26b5925240668c7aa4868f60ad
Patil, Hanamant <hanamant5496@gmail.com>
2025-07-12 08:59:43 +0530

    production managemenet

---
+++ Assignment7/Controllers/productionstatusController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class productionstatusController : Controller
+    {
+        public IActionResult productionstatus()
+        {
+            return View();
+        }
+    }
+}
