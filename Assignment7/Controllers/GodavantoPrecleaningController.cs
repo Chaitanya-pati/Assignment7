c515d7265786e2019cbcd877536ffe541e820584
Patil, Hanamant <hanamant5496@gmail.com>
2025-07-23 09:44:53 +0530

    update

---
+++ Assignment7/Controllers/GodavantoPrecleaningController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class GodavantoPrecleaningController : Controller
+    {
+        public IActionResult GodavantoPrecleaning()
+        {
+            return View();
+        }
+    }
+}
