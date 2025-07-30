b29061705c58592672299b72e24468e9b642db26
Patil, Hanamant <hanamant5496@gmail.com>
2025-07-30 17:08:54 +0530

    salesmanagemenet completed

---
+++ Assignment7/Controllers/SalesDispatchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment7.Controllers
+{
+    public class SalesDispatchController : Controller
+    {
+        public IActionResult SalesDispatch()
+        {
+            return View();
+        }
+    }
+}
